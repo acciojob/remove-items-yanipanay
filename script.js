ae486d15983920e1b181a6c82bda2826bd664dd5
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-10-11 00:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const button = document.querySelector("input");
+button.addEventListener("click",handleClick);
+
+const options = document.getElementById("colorSelect");
+
+function handleClick(event){
+	event.preventDefault();
+	options.remove(options.selectedIndex);
+}
